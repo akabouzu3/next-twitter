c856cf3b904446beb3c10598069ffb9ad2d96986
akase <akase@example.com>
2026-03-28 16:18:06 +0900

    さらに表示を非表示

--- src/features/app/components/RightSidebar.tsx
+++ src/features/app/components/RightSidebar.tsx
@@ -262,9 +262,9 @@ export default function RightSidebar({ recommendUsers }: Props) {
               ))}
             </div>
 
-            <Link href="/app/recommended_users" className="mt-2 flex w-full">
+            {/* <Link href="/app/recommended_users" className="mt-2 flex w-full">
               <span className="text-blue-300/80">さらに表示</span>
-            </Link>
+            </Link> */}
           </section>
         </div>
       </div>
