e7018771b853607a55e703902f8f46072830a645
akase <akase@example.com>
2026-04-27 16:05:47 +0900

    UserPageHeaderコンポーネント実装完了完了

--- src/app/(protected)/users/[username]/_components/UserPageHeader.tsx
+++ src/app/(protected)/users/[username]/_components/UserPageHeader.tsx
@@ -1,4 +1,11 @@
-import { Search } from "lucide-react";
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import {
+  ArrowLeft, 
+  // Search,
+ } from "lucide-react";
 import { UserProfile } from "@/features/user/server/get-user";
 
 type Props = {
@@ -8,16 +15,30 @@ type Props = {
 export default function UserPageHeader({
   user,
 }: Props) {
+
+  const router = useRouter();
+
+  const handleBack = (e: React.MouseEvent) => {
+    e.preventDefault();
+
+    // 履歴があるかチェック
+    if (window.history.length > 1) {
+      router.back();
+    } else {
+      router.push("/app"); // fallback
+    }
+  };
+
   return (
-    <header className="sticky top-0 z-20 flex h-[53px] items-center justify-between border-b border-white/10 bg-black/80 px-4 backdrop-blur-md">
+    <header className="sticky top-0 z-20 flex h-[53px] items-center justify-between border-b border-white/10 bg-black/80 px-2 backdrop-blur-md">
       <div className="flex items-center gap-6">
-        <button
-          type="button"
-          aria-label="戻る"
-          className="grid size-9 place-items-center rounded-full transition hover:bg-white/10"
+        <Link
+          href="/home" // fallback
+          onClick={handleBack}
+          className="grid size-9 place-items-center rounded-full hover:bg-white/10"
         >
-          ←
-        </button>
+          <ArrowLeft className="size-5" />
+        </Link>
 
         <div>
           <h1 className="text-xl font-bold leading-6">
@@ -29,13 +50,13 @@ export default function UserPageHeader({
         </div>
       </div>
 
-      <button
+      {/* <button
         type="button"
         aria-label="検索"
-        className="grid size-9 place-items-center rounded-full transition hover:bg-white/10"
+        className="grid size-9 place-items-center rounded-full transition hover:bg-white/10 cursor-pointer"
       >
         <Search className="size-5" />
-      </button>
+      </button> */}
     </header>
     
   )
